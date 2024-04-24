1dace448e82300702da3a85cd0484f7998bac132
Alex Esquerdo <alexqgon93@gmail.com>
2024-04-24 20:49:33 +0200

    feat: refactor models to use one instead of 3 equally fro url

--- app/src/main/java/com/alexesquerdo/giphy_app/network/responses/common/NetworkUrl.kt
+++ app/src/main/java/com/alexesquerdo/giphy_app/network/responses/common/NetworkUrl.kt
@@ -1,11 +1,11 @@
-package com.alexesquerdo.giphy_app.network.responses.trending
+package com.alexesquerdo.giphy_app.network.responses.common
 
 
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-data class NetworkOnLoad(
+data class NetworkUrl(
     @Json(name = "url")
     val url: String
 )
--- app/src/main/java/com/alexesquerdo/giphy_app/network/responses/trending/NetworkAnalytics.kt
+++ app/src/main/java/com/alexesquerdo/giphy_app/network/responses/trending/NetworkAnalytics.kt
@@ -1,15 +1,16 @@
 package com.alexesquerdo.giphy_app.network.responses.trending
 
 
+import com.alexesquerdo.giphy_app.network.responses.common.NetworkUrl
 import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class NetworkAnalytics(
     @Json(name = "onclick")
-    val onClick: NetworkOnClick,
+    val onClick: NetworkUrl,
     @Json(name = "onload")
-    val onLoad: NetworkOnLoad,
+    val onLoad: NetworkUrl,
     @Json(name = "onsent")
-    val onSent: NetworkOnSent
+    val onSent: NetworkUrl
 )
