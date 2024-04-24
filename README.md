c23b22a28597541413b9f4b76a81d04934aeb7c3
Alex Esquerdo <alexqgon93@gmail.com>
2024-04-24 18:12:38 +0200

    feat: initial commit with the dependencies added.

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# giphy-app
-Android app repository integrates Hillt DI, Clean Architecture with MVVM, and RxJava/RxKotlin for reactive programming. This setup ensures efficient dependency management, modularity, and responsiveness in app development.  Resources
+# 🤖 GIPHY APP ANDROID
+## What is this repository for?
+
+Android app repository integrates Hilt DI, Clean Architecture with MVVM, and RxJava/RxKotlin for reactive programming. This setup ensures efficient dependency management, modularity, and responsiveness in app development.
+
+## Technology Stack
+
+|                | **Name** | **Version** |  
+|----------------|----------|-------------|  
+| **Lenguage**   | Kotlin   | 1.9.0       |  
+| **Build Tool** | Gradle   | 8.3.1       |  
+
+
+## 🛠 Setup & Installation
+* This app is and app which uses the [Giphy Api](https://developers.giphy.com/docs/api/#quick-start-guide) to be able to show trending gifs.
+* To be able to execute this app it will have the necessity to create an account and obtain an API KEY.
+* Finally on local.properties add this line:
+```  
+api_key=<Your Api Key>  
+```  
+
+## Libraries used on this project
+- [Kotlin](https://kotlinlang.org/)
+- [JetpackCompose](https://developer.android.com/jetpack/compose)
+- [Hilt](https://developer.android.com/training/dependency-injection/hilt-android)
+- [ViewModel](https://developer.android.com/topic/libraries/architecture/viewmodel)
+- [Coroutines](https://github.com/Kotlin/kotlinx.coroutines)
+- [Flow](https://kotlin.github.io/kotlinx.coroutines/kotlinx-coroutines-core/kotlinx.coroutines.flow/)
+- [Material Design 3](https://developer.android.com/jetpack/compose/designsystems/material3)
+- [KSP](https://github.com/google/ksp)
+- [JUnit](https://developer.android.com/training/testing/local-tests)
+- [Mockk](https://mockk.io)
+- [Secrets Gradle Plugin for Android](https://github.com/google/secrets-gradle-plugin)
---
+++ app/build.gradle.kts
@@ -0,0 +1,89 @@
+import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
+
+plugins {
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    alias(libs.plugins.secrets)
+    alias(libs.plugins.hilt)
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.alexesquerdo.giphy_app"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.alexesquerdo.giphy_app"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.11"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+}
+
+
+val localProperties = gradleLocalProperties(rootDir, providers)
+val apikey: String = localProperties.getProperty("api_key")
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    implementation(libs.hilt.android)
+    implementation(libs.logging.interceptor)
+    implementation(libs.retrofit)
+    implementation(libs.moshi)
+    implementation(libs.moshi.kotlin)
+    implementation(libs.converter.moshi)
+    implementation(libs.arrow.core)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    ksp(libs.hilt.android.compiler)
+    ksp(libs.moshi.kotlin.codegen)
+    testImplementation(libs.junit)
+    testImplementation(libs.mockk)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+}
