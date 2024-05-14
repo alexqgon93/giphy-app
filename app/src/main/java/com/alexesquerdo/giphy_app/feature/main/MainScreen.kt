4ee6c4c294e4b24fa40733e856f58ceb2cfccd03
Alex Esquerdo <alex.esquerdo@the-cocktail.com>
2024-05-14 14:02:11 +0200

    feat: add new fab on MainScreen.

--- app/src/main/java/com/alexesquerdo/giphy_app/feature/main/MainScreen.kt
+++ app/src/main/java/com/alexesquerdo/giphy_app/feature/main/MainScreen.kt
@@ -1,16 +1,25 @@
 package com.alexesquerdo.giphy_app.feature.main
 
+import android.content.Context
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.app.ShareCompat
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.alexesquerdo.giphy_app.R
 import com.alexesquerdo.giphy_app.components.molecules.header.HeaderMolecule
 import com.alexesquerdo.giphy_app.components.molecules.header.HeaderMoleculeModel
+import com.alexesquerdo.giphy_app.domain.models.GiphyItem
 import com.alexesquerdo.giphy_app.feature.home.HomeViewModel
 import com.alexesquerdo.giphy_app.navigation.main.NavigationMainHost
 
@@ -26,6 +35,7 @@ internal fun MainScreen(
     screenState: MainScreenState = rememberMainScreenState(),
 ) {
     val state by viewModel.state.collectAsState()
+    val context = LocalContext.current
     Scaffold(
         topBar = {
             HeaderMolecule(
@@ -47,5 +57,26 @@ internal fun MainScreen(
                 )
             }
         },
+        floatingActionButton = {
+            if (screenState.showUpNavigation) {
+                FloatingActionButton(onClick = {
+                    state.itemSelected?.let {
+                        shareGif(
+                            context = context,
+                            gitItem = it
+                        )
+                    }
+                }) {
+                    Icon(imageVector = Icons.Default.Share, contentDescription = "Share Gif")
+                }
+            }
+        },
+        floatingActionButtonPosition = FabPosition.Center,
     )
 }
+
+private fun shareGif(context: Context, gitItem: GiphyItem) = ShareCompat
+    .IntentBuilder(context)
+    .setType("text/plain")
+    .setSubject(gitItem.title)
+    .setText(gitItem.url).intent.also(context::startActivity)
